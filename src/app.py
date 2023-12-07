b56680f387d11ede0cc8cc9c8a05ef18e0e0d138
Seth Emery <37260478+collaboratorsje@users.noreply.github.com>
2023-12-07 15:43:49 -0600

    Flask overwrite

---
+++ src/app.py
@@ -0,0 +1,140 @@
+from flask import Flask, render_template, request, jsonify
+import csv
+from flask_cors import CORS  # Import CORS from flask_cors
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for your Flask app
+
+class ChatBot:
+    def __init__(self):
+        self.user_name = ""
+        self.facility_type = ""
+
+    def handle_message(self, user_message):
+        response = ""
+        if not self.user_name:
+            self.user_name = user_message.strip()
+            response = f"Hello, {self.user_name}! Please select a facility type:"
+        elif not self.facility_atype:
+            self.facility_type = user_message.strip()
+            response = f"You selected: {self.facility_type}. Please enter your postal code:"
+        elif user_message.isdigit() and len(user_message) == 6:
+            info = self.display_information(self.facility_type, user_message)
+            if info:
+                response = info
+                response += "\nDo you want to have another interaction? (yes/no):"
+            else:
+                response = "No information found for the selected facility type and postal code."
+        else:
+            response = "Invalid input. Please enter valid details as requested."
+        return response
+
+    def display_information(self, facility_type, postal_code):
+        matching_info = []
+        with open('data_clean/clean_healthCare_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header row
+            for row in reader:
+                if row[10] == facility_type and row[5] == postal_code:
+                    matching_info.append({
+                        'Hospital Name': row[0],
+                        'Hospital Type': row[1],
+                        'Address': row[2],
+                        'City': row[3],
+                        'State': row[4],
+                        'Postal Code': row[5],
+                        'Email': row[6],
+                        'Contact Number': row[7],
+                        'Rating': row[8],
+                        'Doctor\'s Name': row[9],
+                        'Insurance': row[11],
+                        'Emergency Services': row[12],
+                        'Opening Hours': row[13],
+                        'Website URL': row[16]
+                    })
+
+        if matching_info:
+            matching_info = sorted(matching_info, key=lambda x: float(x['Rating']), reverse=True)
+            info = "Here is information for your selection:\n"
+            for row in matching_info:
+                info += '\n'
+                info += f"Hospital Name: {row['Hospital Name']}\n"
+                info += f"Hospital Type: {row['Hospital Type']}\n"
+                info += f"Address: {row['Address']}\n"
+                info += f"City: {row['City']}\n"
+                info += f"State: {row['State']}\n"
+                info += f"Postal Code: {row['Postal Code']}\n"
+                info += f"Email: {row['Email']}\n"
+                info += f"Contact Number: {row['Contact Number']}\n"
+                info += f"Rating: {row['Rating']}\n"
+                info += f"Doctor's Name: {row['Doctor''s Name']}\n"
+                info += f"Insurance: {row['Insurance']}\n"
+                info += f"Emergency Services: {row['Emergency Services']}\n"
+                info += f"Opening Hours: {row['Opening Hours']}\n"
+                info += f"Website URL: {row['Website URL']}\n" 
+            return info
+        else:
+            return None
+
+chatbot = ChatBot()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/map')  # Define a new route for the map view
+def map_view():
+    return render_template('map.html')  # Serve the map.html file when the user navigates to /map_view
+
+@app.route('/submit_message', methods=['POST'])
+def submit_message():
+    user_message = request.form['user_message']
+    response = chatbot.handle_message(user_message)
+    return jsonify({'bot_response': response})
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    facility_type = request.args.get('facility_type')
+    postal_code = request.args.get('postal_code')
+    #print(facility_type)
+    #print(postal_code)
+    
+    matching_info = []
+    try:
+        #print("file before")
+        with open('data_clean/clean_healthCare_data.csv', 'r') as file:
+            #print("file oppened")
+            reader = csv.reader(file)
+            next(reader)  # Skip the header row
+            for row in reader:
+                print(row[10], facility_type, len(row[10]), len(facility_type))
+                if row[10] == facility_type and row[5] == postal_code:
+                    matching_info.append({
+                        'Hospital Name': row[0],
+                        'Hospital Type': row[1],
+                        'Address': row[2],
+                        'City': row[3],
+                        'State': row[4],
+                        'Postal Code': row[5],
+                        'Email': row[6],
+                        'Contact Number': row[7],
+                        'Rating': row[8],
+                        'Doctor\'s Name': row[9],
+                        'Insurance': row[11],
+                        'Emergency Services': row[12],
+                        'Opening Hours': row[13],
+                        'Website URL': row[16]
+                    })
+
+        if matching_info:
+            matching_info = sorted(matching_info, key=lambda x: float(x['Rating']), reverse=True)
+            return jsonify({'data': matching_info})
+        else:
+            return jsonify({'data': []})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # Return an error message with HTTP status 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
+
+    
---
+++ src/data_preprocessing.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import csv
+
+# Load the Excel file into a Pandas DataFrame
+excel_file = 'data_raw/raw_healthCare_data.xlsx'  # Replace with your Excel file name
+data = pd.read_excel(excel_file)
+
+# Save the DataFrame to a CSV file
+csv_file = 'data_raw/raw_healthCare_data.csv'  # Replace with your desired CSV file name
+data.to_csv(csv_file, index=False, encoding='utf-8-sig')  # Set index=False to avoid writing row indices
+
+
+# Load the CSV file into a pandas DataFrame with the specified encoding
+file_path = 'data_raw/raw_healthCare_data.csv'
+encoding = 'utf-8-sig'  # Replace with the appropriate encoding
+df_health = pd.read_csv(file_path, encoding=encoding)
+
+df_health = df_health.replace('’', "'", regex=True)
+df_health.to_csv(csv_file, index=False, encoding='utf-8')
+
+print(df_health.head())
+
+# Count and print the missing values before replacement with NA
+missing_values = df_health.isna().sum()
+print("Missing values before replacement:")
+print(missing_values)
+
+# Replace missing values with "NA"
+df_health.fillna("NA", inplace=True)
+
+# Count and print the missing values after replacement with NA
+missing_values_after = df_health.isna().sum()
+print("Missing values after replacement:")
+print(missing_values_after)
+
+# Remove leading and trailing spaces from object columns using str.strip() function
+df_health_obj = df_health.select_dtypes(['object'])
+df_health[df_health_obj.columns] = df_health_obj.apply(lambda x: x.str.strip())
+
+# Renamimg the columns with the meaningful names
+df_health.rename(columns={'Hospital Name': 'hospital_name',
+                   'Hospital Type ': 'hospital_type',
+                   'Postal Code': 'postal_code',
+                   "Doctor's Name": 'doctor_name',
+                   'Facility Type ': 'facility_type',
+                    'Emergency Service' : 'emergency_services',
+                    'Opening Hours' : 'opening_hours',
+                    'Web site URL' : 'website_url'}, inplace=True)
+print(df_health.head())
+
+# Display the DataFrame without the "S.No" column
+#df_health = df_health.drop(columns=["S.No"])
+
+# Save the cleaned DataFrame directly to a new CSV file, strip leading spaces during save
+output_file = 'data_clean/clean_healthCare_data.csv'
+df_health.to_csv(output_file, index=False, sep=',', quoting=csv.QUOTE_MINIMAL, quotechar='"', escapechar='\\', encoding='utf-8-sig')
+
+print(f"Cleaned data saved to {output_file}")
+     
+
+
+# In[ ]:
+
+
+
+
